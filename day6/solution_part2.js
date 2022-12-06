dcaa5efefd2a225e088dfaa70b1a78882fb29af3
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2022-12-06 11:33:20 +0100

    feat: day6

---
+++ day6/solution_part2.js
@@ -0,0 +1,26 @@
+const file = 'input.txt'
+const fs = require('fs')
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return false
+  }
+
+  // Parse data
+  const rows = data.split('\n').filter(item => item)
+    .map(item => {
+      const chars = item.split('')
+      let value = 0
+      for (let i = 13; i < chars.length; i++) {
+        const temp = chars.slice(i - 13, i + 1)
+        if (temp.length === [...new Set(temp)].length) {
+          value = i
+          break
+        }
+      }
+      return value + 1
+    })
+
+  console.log(rows[0])
+})
